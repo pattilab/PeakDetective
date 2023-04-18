ce6bcb5b7a1dbfba101cb6306a678f336d152ca2
e-stan <estancl1234@gmail.com>
2023-04-18 16:34:07 -0500

    added files

---
+++ workbooks/IPO_run_yeast_spikein.R
@@ -0,0 +1,39 @@
+library("xcms")
+library("CAMERA")
+library("IPO")
+
+pathData <- "D:/PeakDetective/data/yeast_spike_in/" #path to your .mzML 
+
+
+setwd(pathData)
+files = list.files(pattern = "*.mzML")
+numFiles = length(files)
+
+peakpickingParameters <- getDefaultXcmsSetStartingParams('centWave')
+peakpickingParameters$min_peakwidth <- c(10, 30)
+peakpickingParameters$max_peakwidth <- c(30,100)
+peakpickingParameters$ppm <- c(5,30)
+peakpickingParameters$snthresh <- c(1,10)
+peakpickingParameters$noise <- c(100,10000)
+peakpickingParameters$prefilter <- c(1,5)
+
+
+time.xcmsSet <- system.time({ # measuring time
+  resultPeakpicking <- 
+    optimizeXcmsSet(files = files, 
+                    params = peakpickingParameters, 
+                    nSlaves = 10, 
+                    subdir = NULL,
+                    plot = TRUE)
+})
+  
+
+ppm = resultPeakpicking$best_settings$parameters$ppm
+minwidth = resultPeakpicking$best_settings$parameters$min_peakwidth
+maxWidth = resultPeakpicking$best_settings$parameters$max_peakwidth
+noise = resultPeakpicking$best_settings$parameters$noise
+s2n = resultPeakpicking$best_settings$parameters$snthresh
+mzDiff = resultPeakpicking$best_settings$parameters$mzdiff
+prefilter = resultPeakpicking$best_settings$parameters$prefilter
+
+
